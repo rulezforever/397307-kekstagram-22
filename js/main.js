1495b0037d516401135a0abe94eb79cd029b2abe
Ivan Semenov <i.semenov@icloud.com>
2021-01-31 12:17:06 +0300

    fix ошибок линтера

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 const getRandomIntInclusive = function (min, max){
   if (max <= min) {
-    console.log("Неправильный диапазон");
-  } else if (max < 0 || min < 0){
-    console.log("Диапазон отрицательный");
+    alert('Неправильный диапазон');
+  } else if (max < 0 || min < 0) {
+    alert('Диапазон отрицательный');
   } else {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
+getRandomIntInclusive(5, 10);
+
+
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 const checkLength = function(str, maxLength){
   return str.length <= maxLength
 }
+
+checkLength('hello', 5);
