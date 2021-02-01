9ca2c3b46889cfeee99fc793da7d47017760de9c
Ivan Semenov <i.semenov8@yandex.ru>
2021-02-01 09:35:29 +0300

    Исправил замечания

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const getRandom = function(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+
 const getRandomIntInclusive = function (min, max){
   if (max <= min) {
-    alert('Неправильный диапазон');
-  } else if (max < 0 || min < 0) {
-    alert('Диапазон отрицательный');
-  } else {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    alert('min должен быть меньше max');
   }
+  if (max < 0 || min < 0) {
+    alert('min и max должны быть больше нуля');
+  }
+  return Math.floor(getRandom(min, max));
 }
+
+getRandom(5, 10);
 getRandomIntInclusive(5, 10);
-
-
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 const checkLength = function(str, maxLength){
   return str.length <= maxLength
