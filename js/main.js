b8054906c8327dc3f95c1075efb42df4810cffd9
Ivan Semenov <i.semenov8@yandex.ru>
2021-02-03 15:43:29 +0300

    module3-task1

--- js/main.js
+++ js/main.js
@@ -18,3 +18,59 @@ getRandomIntInclusive(5, 10);
 const checkLength = (str, maxLength) => str.length <= maxLength;
 
 checkLength('hello', 5);
+
+const POST_COUNT = 25;
+const AVATAR_COUNT = 6;
+const COMMENTS_COUNT = 10;
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+];
+
+const ID = Array.from({ length: 25 }, (_, i) =>  i + 1);
+
+const DESCRIPTION = 'Описание';
+
+const getRandomArrayElement = (elemets) => {
+  // eslint-disable-next-line no-undef
+  return elemets[_.random(0, elemets.length - 1)];
+}
+
+const createRandomComment = () => {
+  const randomAvatar = getRandomIntInclusive(1, AVATAR_COUNT);
+  const randomId = getRandomIntInclusive(1, 500);
+  return {
+    id: randomId,
+    avatar: `img/avatar-${randomAvatar}.svg`,
+    message: getRandomArrayElement(MESSAGE),
+    name: getRandomArrayElement(NAMES),
+  };
+}
+
+const createPost = () => {
+  const comments = new Array(getRandomIntInclusive(1, COMMENTS_COUNT)).fill(null).map(() => createRandomComment());
+  return {
+    id: getRandomArrayElement(ID),
+    url: `photos/${getRandomArrayElement(ID)}.jpg`,
+    description: DESCRIPTION,
+    likes: getRandomIntInclusive(15,200),
+    comments: comments,
+  };
+};
+
+const posts = new Array(POST_COUNT).fill(null).map(() => createPost());
+// eslint-disable-next-line no-console
+console.log(posts);
+// подскажи, как придумать функцию, чтобы ID не повторялись?
