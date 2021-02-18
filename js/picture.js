efcba47540e7328b0d9a5a62b8a5bffec7f283a1
Ivan Semenov <i.semenov@icloud.com>
2021-02-18 15:44:43 +0300

    исправил comment

--- js/picture.js
+++ js/picture.js
@@ -8,7 +8,7 @@ posts.forEach(({ url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   postFragment.appendChild(pictureElement);
 });
 
