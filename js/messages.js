8c38fd076a3cc2f0cc50d009b92313a8c3def801
Ivan Semenov <i.semenov@icloud.com>
2021-03-10 19:29:52 +0300

    добавил закрытие по клику + Esc + рефактор

--- js/messages.js
+++ js/messages.js
@@ -1,5 +1,5 @@
 import { closePopUp } from './upload-picture.js';
-import { hideElement } from './util.js';
+import { ESCAPE, hideElement } from './util.js';
 
 const successMessageTemplate = document.querySelector('#success').content.querySelector('.success');
 const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
@@ -20,37 +20,48 @@ const getErrorMessageFragment = () => {
   return errorMessageFragment;
 }
 
-const showMessage = (message) => {
-  if (message === 'Error') {
-    main.appendChild(getErrorMessageFragment());
-    const errorButton = document.querySelector('.error__button');
-    const errorSection = document.querySelector('.error');
-    const closeErrorMessage = () => {
-      hideElement(errorSection);
-      errorButton.removeEventListener('click', closeErrorMessage);
-    }
-    errorButton.addEventListener('click', closeErrorMessage)
-  }
-  if (message === 'Success') {
-    main.appendChild(getSuccessMessageFragment());
-    const successButton = document.querySelector('.success__button');
-    const successSection = document.querySelector('.success');
-    const closeSuccessMessage = () => {
-      hideElement(successSection);
-      successButton.removeEventListener('click', closeSuccessMessage);
-    }
-    successButton.addEventListener('click', closeSuccessMessage);
-  }
-}
-
 const renderSuccess = () => {
-  showMessage('Success');
+  main.appendChild(getSuccessMessageFragment());
+  const successButton = document.querySelector('.success__button');
+  const successSection = document.querySelector('.success');
+  const closeSuccessMessage = () => {
+    hideElement(successSection);
+    successButton.removeEventListener('click', closeSuccessMessage);
+  }
+  successButton.addEventListener('click', closeSuccessMessage);
   closePopUp();
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === ESCAPE) {
+      closeSuccessMessage();
+    }
+  });
+  document.addEventListener('click', (evt) => {
+    if (evt.target === successSection) {
+      closeSuccessMessage();
+    }
+  });
 }
 
 const renderError = () => {
-  showMessage('Error');
+  main.appendChild(getErrorMessageFragment());
+  const errorButton = document.querySelector('.error__button');
+  const errorSection = document.querySelector('.error');
+  const closeErrorMessage = () => {
+    hideElement(errorSection);
+    errorButton.removeEventListener('click', closeErrorMessage);
+  }
+  errorButton.addEventListener('click', closeErrorMessage);
   closePopUp();
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === ESCAPE) {
+      closeErrorMessage();
+    }
+  });
+  document.addEventListener('click', (evt) => {
+    if (evt.target === errorSection) {
+      closeErrorMessage();
+    }
+  });
 }
 
 export { renderSuccess, renderError }
